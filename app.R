525491cea427f73ce12fcbfa7fdda6cdf0875fdd
spatiux <76555379+spatiux@users.noreply.github.com>
2020-12-28 11:43:20 +0300

    Update app.R

--- app.R
+++ app.R
@@ -8,7 +8,7 @@ mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
 
 ui <- fluidPage(
 
-  titlePanel("Taylan Deniz Tuncer - Miles Per Gallon"),
+  titlePanel("Taylan Deniz Tuncer - Homework #3: Miles Per Gallon"),
 
   sidebarLayout(
 
